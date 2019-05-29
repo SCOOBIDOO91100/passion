caaae9612dc560673545b11463abf373e445720c
Nabil Hafid Alaoui <nabil.hafidalaoui@gmail.com>
2019-05-29 11:52:27 +0200

    commit

---
+++ nabil.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+  $('.slider-container').unslider({
+    'arrows': false,
+    'nav': false,
+    'animation': 'vertical',
+    'autoplay': true,
+    'delay': 5000
+  });
+});
